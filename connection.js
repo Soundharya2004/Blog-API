14f1a8a0e40d9d35ddb2e7fb648a7f4009c42829
NirmalNagaraj <nirmalns@snaptechnologies.in>
2024-05-03 02:12:33 +0530

    env

--- connection.js
+++ connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
-
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', 
-  password: 'Beelzebub', 
-  database: 'blog',
-  port:3308 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT || 3306 // Default port 3306 if not specified
 });
 
 // Connect to the database
