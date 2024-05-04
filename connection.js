a1ae75d9c40dcabc3ab0f3b5cf07cd6ea58898ae
NirmalNagaraj <nirmalns@snaptechnologies.in>
2024-05-04 19:59:00 +0530

    react-scripts

--- connection.js
+++ connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
-  port: process.env.DB_PORT || 3306 // Default port 3306 if not specified
+
 });
  
 // Connect to the database
